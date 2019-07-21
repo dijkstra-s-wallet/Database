cab79213bf9bdeb0097f2249e8fdebba4b3bac0b
Charlene Nicer <charlenenicer@gmail.com>
2019-07-20 18:19:47 -0700

    fixed additional rows on feature row and added encoding on binFile.py

--- binFile.py
+++ binFile.py
@@ -3,16 +3,16 @@ import numpy as np
 
 
 
-df = pd.read_csv('Canadian_Creditcard.csv')
-binFile = pd.get_dummies(df, columns = ['Rewards', 'Charge/Credit'])
-df = df.drop(['Rewards', 'Charge/Credit'], axis=1)
-df['Sign-Up Bonus'] = df['Sign-Up Bonus'].str.replace(',', '')
-df['Sign-Up Bonus'] = pd.to_numeric(df['Sign-Up Bonus'], errors='coerce')
-df.loc[df['Sign-Up Bonus'] > 0, 'Sign-Up Bonus Bin'] = 1
-df.loc[df['Sign-Up Bonus'] <= 0, 'Sign-Up Bonus Bin'] = 0
-df['Sign-Up Bonus'].apply(str)
-df = df.drop(['Sign-Up Bonus'], axis=1)
-df['Sign-Up Bonus Bin'].apply(str)
+df = pd.read_csv('confirm_database.csv', encoding = "ISO-8859-1")
+binFile = pd.get_dummies(df, columns = ['Reward Program', 'Charge or Credit Card?'])
+#df = df.drop(['Reward Program', 'Charge or Credit Card?'], axis=1)
+#df['Sign-Up Bonus'] = df['Sign-Up Bonus'].str.replace(',', '')
+#df['Sign-Up Bonus'] = pd.to_numeric(df['Sign-Up Bonus'], errors='coerce')
+#df.loc[df['Sign-Up Bonus'] > 0, 'Sign-Up Bonus Bin'] = 1
+#df.loc[df['Sign-Up Bonus'] <= 0, 'Sign-Up Bonus Bin'] = 0
+#df['Sign-Up Bonus'].apply(str)
+#df = df.drop(['Sign-Up Bonus'], axis=1)
+#df['Sign-Up Bonus Bin'].apply(str)
 
 df_merged = pd.merge(df, binFile)
 print(df_merged)
